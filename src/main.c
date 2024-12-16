78ce1a9865a200c68a2e31ac3c93acbd0c0cd8eb
vadniks <whatever@fake.some>
2024-12-16 22:54:15 +0300

    LVGL

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <lvgl/lvgl.h>
 #include "defs.h"
 
-static const int WIDTH = 1600, HEIGHT = 900;
+static const int WIDTH = 1600, HEIGHT = 900, KEYBOARD_INPUT_BUFFER_SIZE = KEYBOARD_BUFFER_SIZE;
 
 static SDL_Window* gWindow = nullptr;
 static SDL_Renderer* gRenderer = nullptr;
@@ -13,6 +13,10 @@ static byte gBuffer[WIDTH * HEIGHT * LV_COLOR_FORMAT_GET_SIZE(LV_COLOR_FORMAT_AR
 
 static int gMouseX = 0, gMouseY = 0;
 static bool gMousePressed = false;
+static short gMouseWheelDiff = 0;
+static bool gMouseWheelPressed = false;
+static char gKeyboardInputBuffer[KEYBOARD_INPUT_BUFFER_SIZE] = {0};
+static bool gKeyboardDummyRead = false;
 
 static void renderCallback(lv_display_t* display, const lv_area_t*, byte*) {
     void* texturePixels = nullptr;
@@ -39,11 +43,22 @@ static void mouseCallback(lv_indev_t*, lv_indev_data_t* data) {
 }
 
 static void mouseWheelCallback(lv_indev_t*, lv_indev_data_t* data) {
-
+    data->enc_diff = gMouseWheelDiff;
+    data->state = gMouseWheelPressed ? LV_INDEV_STATE_PRESSED : LV_INDEV_STATE_RELEASED;
 }
 
 static void keyboardCallback(lv_indev_t*, lv_indev_data_t* data) {
+    const unsigned long length = SDL_strlen(gKeyboardInputBuffer);
 
+    if (gKeyboardDummyRead) {
+        gKeyboardDummyRead = false;
+        data->state = LV_INDEV_STATE_RELEASED;
+    } else if (length > 0) {
+        gKeyboardDummyRead = true;
+        data->state = LV_INDEV_STATE_PRESSED;
+        data->key = (unsigned) gKeyboardInputBuffer[0];
+        memmove(gKeyboardInputBuffer, gKeyboardInputBuffer + 1, length); // not SDL_memmove (src overflow handled)
+    }
 }
 
 int main(void) {
@@ -120,6 +135,8 @@ int main(void) {
         lv_timer_periodic_handler();
 
         SDL_Event event;
+        bool keyboardEvent = false;
+
         while (SDL_PollEvent(&event) == 1) {
             switch (event.type) {
                 case SDL_QUIT:
@@ -130,15 +147,27 @@ int main(void) {
                     lv_indev_read(mouse);
                     break;
                 case SDL_MOUSEBUTTONDOWN:
-                    if (event.button.button == SDL_BUTTON_LEFT) {
-                        gMousePressed = true;
-                        lv_indev_read(mouse);
+                    switch (event.button.button) {
+                        case SDL_BUTTON_LEFT:
+                            gMousePressed = true;
+                            lv_indev_read(mouse);
+                            break;
+                        case SDL_BUTTON_MIDDLE:
+                            gMouseWheelPressed = true;
+                            lv_indev_read(mouseWheel);
+                            break;
                     }
                     break;
                 case SDL_MOUSEBUTTONUP:
-                    if (event.button.button == SDL_BUTTON_LEFT) {
-                        gMousePressed = false;
-                        lv_indev_read(mouse);
+                    switch (event.button.button) {
+                        case SDL_BUTTON_LEFT:
+                            gMousePressed = false;
+                            lv_indev_read(mouse);
+                            break;
+                        case SDL_BUTTON_MIDDLE:
+                            gMouseWheelPressed = false;
+                            lv_indev_read(mouseWheel);
+                            break;
                     }
                     break;
                 case SDL_WINDOWEVENT:
@@ -146,12 +175,69 @@ int main(void) {
                     lv_indev_read(mouse);
                     break;
                 case SDL_MOUSEWHEEL:
-//                    event.wheel.y > 0 ? up() : down();
+                    gMouseWheelDiff = (short) -(event.wheel.y);
+                    lv_indev_read(mouseWheel);
+                    break;
+                case SDL_KEYDOWN:
+                    unsigned key = 0;
+                    switch (event.key.keysym.sym) {
+                        case SDLK_RIGHT:
+                        case SDLK_KP_PLUS:
+                            key = LV_KEY_RIGHT;
+                        case SDLK_LEFT:
+                        case SDLK_KP_MINUS:
+                            key = LV_KEY_LEFT;
+                        case SDLK_UP:
+                            key = LV_KEY_UP;
+                        case SDLK_DOWN:
+                            key = LV_KEY_DOWN;
+                        case SDLK_ESCAPE:
+                            key = LV_KEY_ESC;
+                        case SDLK_BACKSPACE:
+                            key = LV_KEY_BACKSPACE;
+                        case SDLK_DELETE:
+                            key = LV_KEY_DEL;
+                        case SDLK_KP_ENTER:
+                        case SDLK_RETURN:
+                            key = LV_KEY_ENTER;
+                        case SDLK_TAB:
+                        case SDLK_PAGEDOWN:
+                            key = LV_KEY_NEXT;
+                        case SDLK_PAGEUP:
+                            key = LV_KEY_PREV;
+                        case SDLK_HOME:
+                            key = LV_KEY_HOME;
+                        case SDLK_END:
+                            key = LV_KEY_END;
+                    }
+
+                    if (!key)
+                        break;
+
+                    const int length = (int) SDL_strlen(gKeyboardInputBuffer);
+                    if (length < KEYBOARD_INPUT_BUFFER_SIZE - 1) {
+                        gKeyboardInputBuffer[length] = (char) key;
+                        gKeyboardInputBuffer[length + 1] = 0;
+                    }
+
+                    keyboardEvent = true;
+                    break;
+                case SDL_TEXTINPUT:
+                    if (SDL_strlen(gKeyboardInputBuffer) + SDL_strlen(event.text.text) < KEYBOARD_INPUT_BUFFER_SIZE - 1)
+                        strcat(gKeyboardInputBuffer, event.text.text);
+                    keyboardEvent = true;
                     break;
             }
+
+            if (keyboardEvent) {
+                int length = (int) SDL_strlen(gKeyboardInputBuffer);
+                while (length) {
+                    lv_indev_read(keyboard);
+                    lv_indev_read(keyboard);
+                    length--;
+                }
+            }
         }
-
-
     }
     endLoop:
 
