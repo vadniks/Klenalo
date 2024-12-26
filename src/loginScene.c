931e0eb910989a7b105af37c946be2b113f0fa56
vadniks <whatever@fake.some>
2024-12-26 22:07:59 +0300

    Commit

--- src/loginScene.c
+++ src/loginScene.c
@@ -13,7 +13,7 @@ static lv_obj_t* gPreviousScreen = nullptr;
 static lv_obj_t* gScreen = nullptr;
 static lv_group_t* gGroup = nullptr;
 static lv_obj_t* gWelcomeLabel = nullptr;
-//static lv_obj_t* gAddressTextArea = nullptr;
+static lv_obj_t* gAddressLabel = nullptr;
 static lv_obj_t* gPasswordTextArea = nullptr;
 static lv_obj_t* gRememberCredentialsCheckbox = nullptr;
 static lv_obj_t* gSignInButton = nullptr;
@@ -36,31 +36,30 @@ void loginSceneInit(void) {
     inputAssignToGroup(gGroup);
 
     assert(gWelcomeLabel = lv_label_create(gScreen));
-    lv_obj_set_style_text_font(gWelcomeLabel, resourcesFontLarge(), LV_STYLE_STATE_CMP_SAME);
-    lv_label_set_text(gWelcomeLabel, constsString(WELCOME));
+    lv_obj_set_style_text_font(gWelcomeLabel, resourcesFontLarge(), 0);
+    lv_label_set_text_static(gWelcomeLabel, constsString(WELCOME));
 
-//    assert(gAddressTextArea = lv_textarea_create(gScreen));
-//    lv_textarea_set_one_line(gAddressTextArea, true);
-//    lv_obj_set_style_text_font(gAddressTextArea, resourcesFontNormal(), LV_STYLE_STATE_CMP_SAME);
-//    lv_textarea_set_text(gAddressTextArea, "0.0.0.0"); // TODO: stub, spangroup?
-//    lv_obj_set_state(gAddressTextArea, LV_STATE_DISABLED, true);
+    assert(gAddressLabel = lv_label_create(gScreen));
+    lv_obj_set_style_text_font(gAddressLabel, resourcesFontNormal(), 0);
+    lv_obj_set_style_text_opa(gAddressLabel, 171, 0);
+    lv_label_set_text_static(gAddressLabel, "IP address: 0.0.0.0"); // TODO: stub
 
     assert(gPasswordTextArea = lv_textarea_create(gScreen));
     lv_textarea_set_one_line(gPasswordTextArea, true);
     lv_textarea_set_max_length(gPasswordTextArea, MAX_PASSWORD_SIZE);
     lv_textarea_set_placeholder_text(gPasswordTextArea, constsString(PASSWORD));
-    lv_obj_set_style_text_font(gPasswordTextArea, resourcesFontNormal(), LV_STYLE_STATE_CMP_SAME);
+    lv_obj_set_style_text_font(gPasswordTextArea, resourcesFontNormal(), 0);
     lv_textarea_set_password_mode(gPasswordTextArea, true);
 
     assert(gRememberCredentialsCheckbox = lv_checkbox_create(gScreen));
-    lv_obj_set_style_text_font(gRememberCredentialsCheckbox, resourcesFontNormal(), LV_STYLE_STATE_CMP_SAME);
-    lv_checkbox_set_text(gRememberCredentialsCheckbox, constsString(REMEMBER_CREDENTIALS));
+    lv_obj_set_style_text_font(gRememberCredentialsCheckbox, resourcesFontNormal(), 0);
+    lv_checkbox_set_text_static(gRememberCredentialsCheckbox, constsString(REMEMBER_CREDENTIALS));
 
     assert(gSignInButton = lv_button_create(gScreen));
 
     assert(gSignInLabel = lv_label_create(gSignInButton));
-    lv_obj_set_style_text_font(gSignInLabel, resourcesFontNormal(), LV_STYLE_STATE_CMP_SAME);
-    lv_label_set_text(gSignInLabel, constsString(SIGN_IN));
+    lv_obj_set_style_text_font(gSignInLabel, resourcesFontNormal(), 0);
+    lv_label_set_text_static(gSignInLabel, constsString(SIGN_IN));
     lv_obj_center(gSignInLabel);
 }
 
@@ -74,7 +73,7 @@ void loginSceneQuit(void) {
     lv_obj_delete(gSignInButton);
     lv_obj_delete(gRememberCredentialsCheckbox);
     lv_obj_delete(gPasswordTextArea);
-//    lv_obj_delete(gAddressTextArea);
+    lv_obj_delete(gAddressLabel);
     lv_obj_delete(gWelcomeLabel);
     lv_group_delete(gGroup);
     lv_obj_delete(gScreen);
