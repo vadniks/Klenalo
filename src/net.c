19b3213aacd0e30693ceab2084da070d92e644cc
vadniks <whatever@fake.some>
2025-01-17 23:17:23 +0300

    Commit

--- src/net.c
+++ src/net.c
@@ -17,30 +17,30 @@ void netInit(void) {
     for (; ifaddr; ifaddr = ifaddr->ifa_next) {
         if (ifaddr->ifa_addr->sa_family != AF_INET) continue;
 
-        const unsigned hostAddress = *(unsigned*) (ifaddr->ifa_addr->sa_data + 2);
-        const unsigned subnetMask = *(unsigned*) (ifaddr->ifa_netmask->sa_data + 2);
+        const unsigned hostAddress = be32toh(*(unsigned*) (ifaddr->ifa_addr->sa_data + 2));
+        const unsigned subnetMask = be32toh(*(unsigned*) (ifaddr->ifa_netmask->sa_data + 2));
         const unsigned netAddress = hostAddress & subnetMask;
         const unsigned broadcastAddress = netAddress + ~subnetMask;
-        const unsigned hostMinAddress = netAddress + 0x01000000u; // only for little endian
-        const unsigned hostMaxAddress = broadcastAddress - 0x01000000u;
-        const unsigned hostsCount = htobe32(hostMaxAddress - hostMinAddress) + 1;
+        const unsigned hostMinAddress = netAddress + 1u;
+        const unsigned hostMaxAddress = broadcastAddress - 1u;
+        const unsigned hostsCount = hostMaxAddress - hostMinAddress + 1;
 
         unsigned mask = 0;
-        for (unsigned n = subnetMask; n; n /= 2, mask++);
+        for (unsigned n = be32toh(subnetMask); n; n /= 2, mask++);
 
-        if (hostAddress == 0x0100007f) continue; // loopback
+        if (hostAddress == 0x7f000001) continue; // loopback
 
         printf("%s %s\n", ifaddr->ifa_name, ifaddr->ifa_addr->sa_family == AF_INET ? "ipv4" : "ipv6");
 
-#       define dotNotationLE(x) x & 0xff, (x >> 8) & 0xff, (x >> 16) & 0xff, (x >> 24) & 0xff
-        printf("\tnetwork %u.%u.%u.%u/%u\n", dotNotationLE(netAddress), mask);
-        printf("\tsubnet mask %u.%u.%u.%u\n", dotNotationLE(subnetMask));
-        printf("\tbroadcast %u.%u.%u.%u\n", dotNotationLE(broadcastAddress));
-        printf("\thost %u.%u.%u.%u\n", dotNotationLE(hostAddress));
-        printf("\thost min %u.%u.%u.%u\n", dotNotationLE(hostMinAddress));
-        printf("\thost max %u.%u.%u.%u\n", dotNotationLE(hostMaxAddress));
+#       define dotNotationLEtoBE(x) (x >> 24) & 0xff, (x >> 16) & 0xff, (x >> 8) & 0xff, x & 0xff
+        printf("\tnetwork %u.%u.%u.%u/%u\n", dotNotationLEtoBE(netAddress), mask);
+        printf("\tsubnet mask %u.%u.%u.%u\n", dotNotationLEtoBE(subnetMask));
+        printf("\tbroadcast %u.%u.%u.%u\n", dotNotationLEtoBE(broadcastAddress));
+        printf("\thost %u.%u.%u.%u\n", dotNotationLEtoBE(hostAddress));
+        printf("\thost min %u.%u.%u.%u\n", dotNotationLEtoBE(hostMinAddress));
+        printf("\thost max %u.%u.%u.%u\n", dotNotationLEtoBE(hostMaxAddress));
         printf("\thosts count %d\n", hostsCount);
-#       undef dotNotationLE
+#       undef dotNotationLEtoBE
     }
 
     freeifaddrs(ifaddr);
