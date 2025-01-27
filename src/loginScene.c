08af3f6e8120c1a0beec1e90cd406d70f3c09f56
vadniks <whatever@fake.some>
2025-01-27 23:09:46 +0300

    Sync

--- src/loginScene.c
+++ src/loginScene.c
@@ -10,7 +10,7 @@
 #include "lifecycle.h"
 #include "loginScene.h"
 
-static const int NETS_UPDATE_INTERVAL = 10;
+static const int NETS_UPDATE_INTERVAL = 500;
 
 static atomic bool gInitialized = false;
 
