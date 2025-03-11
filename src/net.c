328eb883169f871ba31582286c33caa8faaac311
vadniks <34599736+vadniks@users.noreply.github.com>
2025-03-11 04:12:33 +0300

    Net

--- src/net.c
+++ src/net.c
@@ -2,6 +2,7 @@
 #include <SDL3/SDL.h>
 #include <SDL3_net/SDL_net.h>
 #include <sys/socket.h>
+#include <netdb.h>
 #include "lifecycle.h"
 #include "net.h"
 
@@ -25,65 +26,41 @@ void netInit(void) {
 
     assert(gMutex = SDL_CreateMutex());
 
-    gSubnetsList = listCreate(false, xfree);
+    gSubnetsList = listCreate(false, nullptr); // TODO: rename to gSubnetsHostsAddressesList
 }
 
 bool netInitialized(void) {
     return gInitialized;
 }
 
-static void fetchSubnets(void) {
-//    assert(lifecycleInitialized() && gInitialized);
-//
-//    SDL_LockMutex(gMutex);
-//    listClear(gSubnetsList);
-//
-//    struct ifaddrs* ifaddrRoot;
-//    assert(!getifaddrs(&ifaddrRoot));
-//
-//    for (struct ifaddrs* ifaddr = ifaddrRoot; ifaddr; ifaddr = ifaddr->ifa_next) {
-//        if (ifaddr->ifa_addr->sa_family != AF_INET) continue;
-//        if ((ifaddr->ifa_flags & IFF_UP) != IFF_UP) continue;
-//
-//        const unsigned hostAddress = swapBytes(*(unsigned*) (ifaddr->ifa_addr->sa_data + 2));
-//        const unsigned subnetMask = swapBytes(*(unsigned*) (ifaddr->ifa_netmask->sa_data + 2));
-//        const unsigned netAddress = hostAddress & subnetMask;
-//        const unsigned broadcastAddress = netAddress + ~subnetMask;
-//
-//        byte mask = 0;
-//        for (unsigned n = subnetMask; n; n & 1 ? mask++ : 0, n >>= 1);
-//
-//        if (hostAddress == 0x7f000001 || (ifaddr->ifa_flags & IFF_LOOPBACK) == IFF_LOOPBACK) continue;
-//
-//        NetSubnet* const net = xmalloc(sizeof *net);
-//        assert(net);
-//        xmemcpy(net, &(NetSubnet) {
-//            {0},
-//            (int) netAddress,
-//            mask,
-//            (int) broadcastAddress,
-//            (int) hostAddress,
-//            (int) ((broadcastAddress - 1u) - (netAddress + 1u) + 1u),
-//            (netAddress & 0xff000000) == 0x0a000000 || (netAddress & 0xfff00000) == 0xac100000 || (netAddress & 0xffff0000) == 0xc0a80000, // private networks https://www.arin.net/reference/research/statistics/address_filters
-//            (ifaddr->ifa_flags & IFF_RUNNING) == IFF_RUNNING
-//        }, sizeof *net);
-//        strncpy((char*) net->name, ifaddr->ifa_name, sizeof(net->name));
-//
-//        assert(hostAddress >= netAddress && hostAddress <= broadcastAddress);
-//        assert(net->hostsCount);
-//
-//        listAddBack(gSubnetsList, net);
-//    }
-//    SDL_UnlockMutex(gMutex);
-//
-//    freeifaddrs(ifaddrRoot);
+static void fetchSubnets(void) { // TODO: this actually fetches hosts' addresses instead of subnets' ones
+    assert(lifecycleInitialized() && gInitialized);
+
+    SDL_LockMutex(gMutex);
+    listClear(gSubnetsList);
+
+    int numAddrs = 0;
+    SDLNet_Address** const addrs = SDLNet_GetLocalAddresses(&numAddrs);
+    assert(numAddrs && addrs);
+
+    for (int i = 0; i < numAddrs; i++) {
+        SDLNet_Address* const addr = addrs[i];
+
+        struct addrinfo* const info = *(struct addrinfo**) ((void*) addr + 32);
+        if (info->ai_addr->sa_family != AF_INET) continue;
+
+        listAddBack(gSubnetsList, (void*) (long) swapBytes(*(unsigned*) ((info->ai_addr->sa_data + 2))));
+    }
+
+    SDLNet_FreeLocalAddresses(addrs);
+    SDL_UnlockMutex(gMutex);
 }
 
 static void* subnetDuplicator(const void* const old) {
     return (void*) old; // addresses themselves are values
 }
 
-List* nullable netSubnets(void) {
+List* nullable netSubnets(void) { // TODO: this actually returns hosts' addresses in those subnets - change namings, like fetchAvailableSubnetsHosts
     SDL_LockMutex(gMutex);
     List* const new = listCopy(gSubnetsList, false, subnetDuplicator);
     SDL_UnlockMutex(gMutex);
@@ -121,7 +98,7 @@ void netStartBroadcastingAndListeningSubnet(const int subnet) {
 
     gSelectedSubnet = subnet;
 
-    SDLNet_Address* const addr = resolveAddress(gSelectedSubnet/*TODO: probably won't work as it's not the host address but a subnet address*/);
+    SDLNet_Address* const addr = resolveAddress(gSelectedSubnet);
     assert(gSubnetBroadcastSocket = SDLNet_CreateDatagramSocket(addr, NET_BROADCAST_SOCKET_PORT));
     SDLNet_UnrefAddress(addr);
 
