2f0fdc5a13dd5129d7a825ec8a0c20de4b179181
vadniks <34599736+vadniks@users.noreply.github.com>
2025-03-15 01:03:23 +0300

    Crypto

--- src/crypto.c
+++ src/crypto.c
@@ -3,13 +3,23 @@
 #include "lifecycle.h"
 #include "crypto.h"
 
+typedef unsigned long long xlong;
+
+const int
+    CRYPTO_PUBLIC_KEY_SIZE = crypto_sign_PUBLICKEYBYTES,
+    CRYPTO_SECRET_KEY_SIZE = crypto_sign_SECRETKEYBYTES,
+    CRYPTO_SIGNATURE_SIZE = crypto_sign_BYTES;
+
 static atomic bool gInitialized = false;
+
+static const byte gMasterPublicKey[CRYPTO_PUBLIC_KEY_SIZE] = "\xa9\x10\x98\xdc\x68\xfb\x26\x29\x71\xbc\x23\x57\x4a\x7\xe7\xc3\x22\x44\x82\x91\xd8\xe4\x3\x88\x82\xad\xbe\x18\xc2\x4e\xef\x77";
+static const byte gMasterSecretKey[CRYPTO_SECRET_KEY_SIZE] = "\xde\x51\x0\xb3\xf3\x68\xf0\x93\x9c\x51\x0\x19\x86\x53\xc3\x99\xc9\xa7\xc2\x23\x9a\xa4\x46\x26\x21\xde\x5\x44\x5c\x4d\x12\x62\xa9\x10\x98\xdc\x68\xfb\x26\x29\x71\xbc\x23\x57\x4a\x7\xe7\xc3\x22\x44\x82\x91\xd8\xe4\x3\x88\x82\xad\xbe\x18\xc2\x4e\xef\x77";
 
 void cryptoInit(void) {
     assert(lifecycleInitialized() && !gInitialized);
     gInitialized = true;
 
-    assert(sodium_init() >= 0);
+    assert(!sodium_init());
 }
 
 bool cryptoInitialized(void) {
@@ -17,7 +27,13 @@ bool cryptoInitialized(void) {
 }
 
 void cryptoMasterSign(const byte* const message, const int size, byte* const buffer) {
+    xlong generatedSize;
+    assert(!crypto_sign(buffer, &generatedSize, message, size, gMasterSecretKey));
+    assert((int) generatedSize == CRYPTO_SIGNATURE_SIZE + size);
+}
 
+bool cryptoCheckMasterSigned(const byte* const signedMessage, const int size) {
+    return !crypto_sign_open(nullptr, nullptr, signedMessage, size, gMasterPublicKey);
 }
 
 void cryptoQuit(void) {
--- src/crypto.h
+++ src/crypto.h
@@ -3,7 +3,10 @@
 
 #include "defs.h"
 
+extern const int CRYPTO_PUBLIC_KEY_SIZE, CRYPTO_SECRET_KEY_SIZE, CRYPTO_SIGNATURE_SIZE;
+
 void cryptoInit(void);
 bool cryptoInitialized(void);
-void cryptoMasterSign(const byte* const message, const int size, byte* const buffer);
+void cryptoMasterSign(const byte* const message, const int size, byte* const buffer); // sizeof(buffer) = signatureSize + size
+bool cryptoCheckMasterSigned(const byte* const signedMessage, const int size); // size = signatureSize + sizeof(message)
 void cryptoQuit(void);
