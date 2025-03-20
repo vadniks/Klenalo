1cd4001c4532ff0ab7b5e061c5182cda2c946c25
vadniks <34599736+vadniks@users.noreply.github.com>
2025-03-21 00:33:47 +0300

    Crypto

--- src/crypto.c
+++ src/crypto.c
@@ -8,13 +8,18 @@ typedef unsigned long long xlong;
 const int
     CRYPTO_SIGN_PUBLIC_KEY_SIZE = crypto_sign_PUBLICKEYBYTES,
     CRYPTO_SIGN_SECRET_KEY_SIZE = crypto_sign_SECRETKEYBYTES,
-    CRYPTO_SIGNATURE_SIZE = crypto_sign_BYTES;
+    CRYPTO_SIGNATURE_SIZE = crypto_sign_BYTES,
+    CRYPTO_ENCRYPT_PUBLIC_KEY_SIZE = crypto_box_PUBLICKEYBYTES, // == crypto_sign_PUBLICKEYBYTES, (1/2)*crypto_sign_SECRETKEYBYTES, crypto_secretbox_KEYBYTES, crypto_kx_PUBLICKEYBYTES, crypto_kx_SECRETKEYBYTES, crypto_kx_SESSIONKEYBYTES, crypto_secretstream_xchacha20poly1305_KEYBYTES
+    CRYPTO_ENCRYPT_SECRET_KEY_SIZE = crypto_box_SECRETKEYBYTES, // == crypto_secretbox_KEYBYTES, crypto_sign_PUBLICKEYBYTES, ...
+    CRYPTO_SEAL_SIZE = crypto_box_SEALBYTES;
 
 static atomic bool gInitialized = false;
 
 // TODO: will be replaced with config file where users can set the keys themselves
 static const byte gMasterSignPublicKey[CRYPTO_SIGN_PUBLIC_KEY_SIZE] = "\xa9\x10\x98\xdc\x68\xfb\x26\x29\x71\xbc\x23\x57\x4a\x7\xe7\xc3\x22\x44\x82\x91\xd8\xe4\x3\x88\x82\xad\xbe\x18\xc2\x4e\xef\x77";
 static const byte gMasterSignSecretKey[CRYPTO_SIGN_SECRET_KEY_SIZE] = "\xde\x51\x0\xb3\xf3\x68\xf0\x93\x9c\x51\x0\x19\x86\x53\xc3\x99\xc9\xa7\xc2\x23\x9a\xa4\x46\x26\x21\xde\x5\x44\x5c\x4d\x12\x62\xa9\x10\x98\xdc\x68\xfb\x26\x29\x71\xbc\x23\x57\x4a\x7\xe7\xc3\x22\x44\x82\x91\xd8\xe4\x3\x88\x82\xad\xbe\x18\xc2\x4e\xef\x77";
+static const byte gMasterSealPublicKey[CRYPTO_ENCRYPT_PUBLIC_KEY_SIZE] = "\x88\x7f\x63\x74\x14\xa1\xb0\xd4\x4e\x18\x49\x85\x33\x4e\x5c\xfe\xda\xff\x8\xbc\x6b\xbb\xe2\xef\x43\x86\xe9\xe7\x5a\x4c\x8\x2f";
+static const byte gMasterSealSecretKey[CRYPTO_ENCRYPT_PUBLIC_KEY_SIZE] = "\x21\xc\xba\xf7\xb3\x4c\xb9\xde\x9b\x31\x6d\xe4\x9f\x20\x6e\x7e\x67\x4e\xaa\x9a\x97\x55\x27\x31\x17\x6b\xb5\xbc\xe\x2b\xe8\xfc";
 
 void cryptoInit(void) {
     assert(lifecycleInitialized() && !gInitialized);
@@ -40,12 +45,12 @@ bool cryptoCheckMasterSigned(const byte* const signedMessage, const int size) {
     return !crypto_sign_open(nullptr, nullptr, signedMessage, size, gMasterSignPublicKey);
 }
 
-void cryptoMasterSeal(const byte* const message, const int size, byte* const sealedMessage) {
-
+void cryptoSeal(const byte* const message, const int size, byte* const sealedMessage, const byte* const sealPublicKey) {
+    assert(!crypto_box_seal(sealedMessage, message, size, sealPublicKey));
 }
 
 bool cryptoMasterUnseal(const byte* const sealedMessage, const int size, byte* const unsealedMessage) {
-
+    return !crypto_box_seal_open(unsealedMessage, sealedMessage, size, gMasterSealPublicKey, gMasterSealSecretKey);
 }
 
 void cryptoQuit(void) {
--- src/crypto.h
+++ src/crypto.h
@@ -3,12 +3,18 @@
 
 #include "defs.h"
 
-extern const int CRYPTO_SIGN_PUBLIC_KEY_SIZE, CRYPTO_SIGN_SECRET_KEY_SIZE, CRYPTO_SIGNATURE_SIZE;
+extern const int
+    CRYPTO_SIGN_PUBLIC_KEY_SIZE,
+    CRYPTO_SIGN_SECRET_KEY_SIZE,
+    CRYPTO_SIGNATURE_SIZE,
+    CRYPTO_ENCRYPT_PUBLIC_KEY_SIZE,
+    CRYPTO_ENCRYPT_SECRET_KEY_SIZE,
+    CRYPTO_SEAL_SIZE;
 
 void cryptoInit(void);
 bool cryptoInitialized(void);
 void cryptoMasterSign(const byte* const message, const int size, byte* const signedMessage); // sizeof(signedMessage) = signatureSize + size
 bool cryptoCheckMasterSigned(const byte* const signedMessage, const int size); // size = signatureSize + sizeof(message)
-void cryptoMasterSeal(const byte* const message, const int size, byte* const sealedMessage);
+void cryptoSeal(const byte* const message, const int size, byte* const sealedMessage, const byte* const sealPublicKey); // sizeof(sealedMessage) = sealSize + size
 bool cryptoMasterUnseal(const byte* const sealedMessage, const int size, byte* const unsealedMessage);
 void cryptoQuit(void);
