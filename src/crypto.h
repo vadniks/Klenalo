e5ff6c75a957902a64749c02a82526d1630c6c71
vadniks <34599736+vadniks@users.noreply.github.com>
2025-03-21 00:35:25 +0300

    Crypto

--- src/crypto.h
+++ src/crypto.h
@@ -16,5 +16,5 @@ bool cryptoInitialized(void);
 void cryptoMasterSign(const byte* const message, const int size, byte* const signedMessage); // sizeof(signedMessage) = signatureSize + size
 bool cryptoCheckMasterSigned(const byte* const signedMessage, const int size); // size = signatureSize + sizeof(message)
 void cryptoSeal(const byte* const message, const int size, byte* const sealedMessage, const byte* const sealPublicKey); // sizeof(sealedMessage) = sealSize + size
-bool cryptoMasterUnseal(const byte* const sealedMessage, const int size, byte* const unsealedMessage);
+bool cryptoMasterUnseal(const byte* const sealedMessage, const int size, byte* const unsealedMessage); // size = sealSize + sizeof(message)
 void cryptoQuit(void);
