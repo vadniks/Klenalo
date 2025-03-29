0b53d711da3e6c5da071909807e133c71b2d1528
vadniks <34599736+vadniks@users.noreply.github.com>
2025-03-30 01:06:51 +0300

    Memory hack - 3rd party function patching

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <string.h>
-#include <dlfcn.h>
 #include <SDL3/SDL.h>
 
 [[gnu::noinline]]
@@ -13,45 +12,23 @@ static bool xSDL_Init(SDL_InitFlags flags) {
     return false;
 }
 
-static void patch_function(void* target_function, void* new_function) {
-    unsigned long page_size = sysconf(_SC_PAGESIZE);
-    void* page_start = (void*) ((unsigned long) target_function & ~(page_size - 1));
+static void patchFunction(void* const original, void* const replacement) {
+    const unsigned long pageSize = sysconf(_SC_PAGESIZE);
+    void* const pageStart = (void*) ((unsigned long) original & ~(pageSize - 1));
 
-    // Change memory permissions to RWX
-    assert(!mprotect(page_start, page_size, PROT_READ | PROT_WRITE | PROT_EXEC));
-    debugArgs("%p %p", new_function, target_function)
-    debug("a")
-    // Overwrite with JMP to our new function
-    byte jmp_code[15] = { 0x48, 0xb8 }; // x86_64 JMP opcode
-//    const long address = 0x5555556cd9c8ul; //(long) 0xffff8000085bcff0ul; //(long) new_function - (long) target_function - 5 - 0x800000000000; //(intptr_t)new_function - (intptr_t)target_function - 5; //(unsigned) ((unsigned long) xSDL_Init & ~((int) 0)); //(unsigned) ((unsigned long) new_function - (unsigned long) target_function);
-//    debugArgs("%lx %ld %u %ld %x", address, address, (unsigned) address, address & 0xffffffff, (int) address)
-    *(unsigned long*) &jmp_code[2] = (unsigned long) new_function;
-    *(unsigned int*) &jmp_code[10] = 0xe0ff;
-    printMemory(jmp_code, sizeof jmp_code, PRINT_MEMORY_MODE_HEX);
+    assert(!mprotect(pageStart, pageSize, PROT_READ | PROT_WRITE | PROT_EXEC));
 
-    memcpy(target_function, jmp_code, sizeof(jmp_code));
+    byte trampoline[12] = {0x48, 0xb8, [10] = 0xff, [11] = 0xe0};
+    *(unsigned long*) &trampoline[2] = (unsigned long) replacement;
+    memcpy(original, trampoline, sizeof(trampoline));
 
-    // Restore memory protection
-    mprotect(page_start, page_size, PROT_READ | PROT_EXEC);
+    assert(!mprotect(pageStart, pageSize, PROT_READ | PROT_EXEC));
 }
 
 int main(void) {
-    debugArgs("%lx %lx %lx %lx", SDL_Init, xSDL_Init, (void*) SDL_Init - (void*) xSDL_Init, (void*) xSDL_Init - (void*) SDL_Init)
-    debugArgs("%lx", max((unsigned long) SDL_Init, (unsigned long) xSDL_Init) - min((unsigned long) SDL_Init, (unsigned long) xSDL_Init))
-    debugArgs("%lu %lu %lu %lu", SDL_Init, xSDL_Init, SDL_Init - xSDL_Init, xSDL_Init - SDL_Init)
-    debugArgs("%lx %lx %lx %lx", main, xSDL_Init, (void*) main - (void*) xSDL_Init, (void*) xSDL_Init - (void*) main)
-    debugArgs("%u %x", (unsigned) ((unsigned long) xSDL_Init - (unsigned long) main), (unsigned) ((unsigned long) xSDL_Init & ~((int) 0)))
-//    assert(0);
-    void* real_func = dlsym(RTLD_NEXT, "SDL_Init"); // Find real function
-    printMemory(SDL_Init, 10, PRINT_MEMORY_MODE_HEX);
-    debugArgs("%lx %p %p", swapBytes((long) SDL_Init), SDL_Init, real_func)
-    if (real_func)
-        patch_function(real_func, (void*) xSDL_Init);
-    printMemory(SDL_Init, 10, PRINT_MEMORY_MODE_HEX);
-    debug("b")
-
+    debugArgs("%p %p", SDL_Init, xSDL_Init)
+    patchFunction((void*) SDL_Init, (void*) xSDL_Init);
     debugArgs("%c", SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) ? 't' : 'f')
-//    assert(0);
     return 0;
 
     const unsigned long allocations = xallocations();
