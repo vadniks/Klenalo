8695a859bde1de59e2f87b13660b80c0305314da
vadniks <34599736+vadniks@users.noreply.github.com>
2025-03-30 02:14:58 +0300

    Cleanup

--- src/main.c
+++ src/main.c
@@ -1,20 +1,7 @@
 
 #include "lifecycle.h"
-#include <stdio.h>
-
-static FILE* xfopen(const char* const filename, const char* const modes) {
-    debugArgs("patched fopen(%s, %s)", filename, modes)
-    return nullptr;
-}
 
 int main(void) {
-    patchFunction((void*) fopen, (void*) xfopen);
-
-    FILE* const self = fopen("/proc/self/exe", "rb");
-    assert(!self);
-
-    return 0;
-
     const unsigned long allocations = xallocations();
     lifecycleInit();
     lifecycleLoop();
