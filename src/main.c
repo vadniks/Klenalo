7f1324025865a08a0a591ac7ebc2408015871598
vadniks <34599736+vadniks@users.noreply.github.com>
2025-03-29 14:26:00 +0300

    Commit

--- src/main.c
+++ src/main.c
@@ -1,31 +1,11 @@
 
-#if TESTING
-#   include <SDL3/SDL.h>
-#   include "list.h"
-#   include "hashtable.h"
-#else
-#   include "lifecycle.h"
-#endif
+#include "lifecycle.h"
 
 int main(void) {
-
-#if TESTING
-    assert(!SDL_Init(0));
-
-    const unsigned long allocations = xallocations();
-
-    listRunTests();
-    hashtableRunTests();
-
-    assert(xallocations() == allocations);
-
-    SDL_Quit();
-#else
     const unsigned long allocations = xallocations();
     lifecycleInit();
     lifecycleLoop();
     lifecycleQuit();
     assert(xallocations() == allocations);
-#endif
     return 0;
 }
