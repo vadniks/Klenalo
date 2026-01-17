d74e348b8405738ebb931ddcb6b7ea0a528a7296
vadniks <34599736+vadniks@users.noreply.github.com>
2026-01-16 23:26:44 -0800

    Unit testing

--- tests/testCrypto.c
+++ tests/testCrypto.c
@@ -27,31 +27,108 @@ static void seal(void) {
 }
 
 static void singleCrypt(void) {
+    CryptoSingleEncryptedBundle* const bundle = xalloca(sizeof *bundle + DATA_SIZE);
+    xmemcpy(bundle->data, DATA, DATA_SIZE);
+    cryptoRandomBytes(bundle->nonce, CRYPTO_SINGLE_CRYPT_NONCE_SIZE);
 
+    cryptoSingleEncrypt(bundle, DATA_SIZE, &SECRET_KEY);
+    assert(cryptoSingleDecrypt(bundle, DATA_SIZE, &SECRET_KEY));
 }
 
 static void streamCrypt(void) {
+    CryptoStreamCoder encoder, decoder;
+    CryptoStreamHeader header;
+    CryptoGenericKey key;
+    cryptoRandomBytes((byte*) &key, CRYPTO_GENERIC_KEY_SIZE);
 
+    cryptoStreamCreateEncoder(&encoder, &header, &key);
+    assert(cryptoStreamCreateDecoder(&decoder, &header, &key));
+
+    const int chunksAmount = 5;
+    CryptoStreamEncryptedChunkBundle* const chunks[chunksAmount] = {
+        xalloca(sizeof *chunks[0] + 5),
+        xalloca(sizeof *chunks[0] + 6),
+        xalloca(sizeof *chunks[0] + 1),
+        xalloca(sizeof *chunks[0] + 1),
+        xalloca(sizeof *chunks[0] + 3),
+    };
+    xmemcpy(chunks[0]->data, "Abcd", 5);
+    xmemcpy(chunks[1]->data, "01234", 6);
+    xmemcpy(chunks[2]->data, " ", 1);
+    xmemcpy(chunks[3]->data, "X", 1);
+    xmemcpy(chunks[4]->data, "\xff\x05\x00", 3);
+
+    cryptoStreamEncrypt(&encoder, chunks[0], 5);
+    cryptoStreamEncrypt(&encoder, chunks[1], 6);
+    cryptoStreamEncrypt(&encoder, chunks[2], 1);
+    cryptoStreamEncrypt(&encoder, chunks[3], 1);
+    cryptoStreamEncrypt(&encoder, chunks[4], 3);
+
+    assert(cryptoStreamDecrypt(&decoder, chunks[0], 5));
+    assert(!xmemcmp(chunks[0]->data, "Abcd", 5));
+    assert(cryptoStreamDecrypt(&decoder, chunks[1], 6));
+    assert(!xmemcmp(chunks[1]->data, "01234", 6));
+    assert(cryptoStreamDecrypt(&decoder, chunks[2], 1));
+    assert(!xmemcmp(chunks[2]->data, " ", 1));
+    assert(cryptoStreamDecrypt(&decoder, chunks[3], 1));
+    assert(!xmemcmp(chunks[3]->data, "X", 1));
+    assert(cryptoStreamDecrypt(&decoder, chunks[4], 3));
+    assert(!xmemcmp(chunks[4]->data, "\xff\x05\x00", 3));
 }
 
 static void nonce(void) {
+    const int size = 16;
+    byte nonce[size];
+    xmemset(nonce + 1, 0xff, size - 1);
+    nonce[0] = 0xfe;
 
+    assert(!cryptoNonceIncrementOverflowChecked(nonce, size));
+    assert(cryptoNonceIncrementOverflowChecked(nonce, size));
 }
 
 static void base64(void) {
+    const byte data[DATA_SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 255, 254};
+    byte data2[DATA_SIZE];
 
+    const int resultSize = cryptoBase64ResultSize(DATA_SIZE);
+    char result[resultSize];
+
+    cryptoBase64Encode(data, DATA_SIZE, result, resultSize);
+    assert((int) xstrnlen(result, 0xff) == resultSize - 1);
+
+    assert(cryptoBase64Decode(result, resultSize, data2, DATA_SIZE) == DATA_SIZE);
+    assert(!xmemcmp(data, data2, DATA_SIZE));
 }
 
 static void padding(void) {
+    const int values[8] = {1, 15, 16, 17, 31, 32, 33, 46};
+    for (int i = 0, size; i < (int) arraySize(values); i++) {
+        size = values[i];
 
+        byte data[size];
+        xmemset(data, 'a', size);
+
+        const int size2 = size + CRYPTO_PADDING_BLOCK_SIZE;
+        byte padded[size2];
+
+        const int actualPaddedSize = cryptoPaddingAdd(padded, size);
+        assert(actualPaddedSize == (size / CRYPTO_PADDING_BLOCK_SIZE + 1) * CRYPTO_PADDING_BLOCK_SIZE);
+        assert(cryptoPaddingRemovedSize(padded, actualPaddedSize) == size);
+    }
 }
 
 static void hash(void) {
+    byte buffer1[CRYPTO_HASH_LARGE_SIZE], buffer2[CRYPTO_HASH_LARGE_SIZE];
+    cryptoHash(nullptr, (byte*) DATA, DATA_SIZE, buffer1, CRYPTO_HASH_LARGE_SIZE);
 
-}
+    CryptoHashState state;
+    cryptoHash(&state, nullptr, 0, nullptr, CRYPTO_HASH_LARGE_SIZE);
+    cryptoHash(&state, (byte*) "Hello ", 6, nullptr, 0);
+    cryptoHash(&state, (byte*) "World!", 6, nullptr, 0);
+    cryptoHash(&state, /*(byte[1]) {0}*/ &(byte) {}, 1, nullptr, 0);
+    cryptoHash(&state, nullptr, 0, buffer2, CRYPTO_HASH_LARGE_SIZE);
 
-static bool lifecycleInitializedInterceptor(void) {
-    return true;
+    assert(!xmemcmp(buffer1, buffer2, CRYPTO_HASH_LARGE_SIZE));
 }
 
 void testCrypto(void) {
@@ -60,6 +137,12 @@ void testCrypto(void) {
 
     sign();
     seal();
+    singleCrypt();
+    streamCrypt();
+    nonce();
+    base64();
+    padding();
+    hash();
 
     cryptoQuit();
 }
