3e5786819490721fa6b19237e7426242fa5dd89e
vadniks <34599736+vadniks@users.noreply.github.com>
2026-01-21 20:42:14 -0800

    Unit testing

--- tests/testCollections.c
+++ tests/testCollections.c
@@ -3,7 +3,118 @@
 #include <time.h>
 #include "../src/collections/list.h"
 
-static Compared listNoDynamicMemoryComparator(const void* const a, const void* const b) {
+static const int LIST_ITEMS_AMOUNT = 10;
+static List* gList = nullptr;
+
+static void ndmInit(void) { // no dynamic memory
+    gList = listCreate(DEFAULT_ALLOCATOR, false, nullptr);
+}
+
+static void ndmAddBack(void) {
+    for (int i = 1; i <= LIST_ITEMS_AMOUNT; i++) {
+        listAddBack(gList, (void*) (long) i);
+        assert(listGet(gList, i - 1) == (void*) (long) i);
+    }
+
+    listClear(gList);
+    assert(!listSize(gList));
+}
+
+static void ndmAddFront(void) {
+    for (int i = 1; i <= LIST_ITEMS_AMOUNT; i++)
+        listAddFront(gList, (void*) (long) i);
+    for (int i = 1; i <= LIST_ITEMS_AMOUNT; i++)
+        assert(listGet(gList, i - 1) == (void*) (long) (LIST_ITEMS_AMOUNT - i + 1));
+}
+
+static void* ndmDuplicator(const void* const value) {
+    return (void*) value;
+}
+
+static void ndmCopy(void) {
+    List* const list2 = listCopy(gList, false, ndmDuplicator);
+    assert(listSize(gList) == listSize(list2));
+
+    for (int i = 0; i < LIST_ITEMS_AMOUNT; i++)
+        assert(listGet(gList, i) == listGet(list2, i));
+
+    listDestroy(list2);
+    listClear(gList);
+}
+
+static void ndmPopFirst(void) {
+    for (int i = 1; i <= LIST_ITEMS_AMOUNT; i++)
+        listAddBack(gList, (void*) (long) i);
+
+    int i = 1, item, j = LIST_ITEMS_AMOUNT;
+    while ((item = (int) (long) listPopFirst(gList))) {
+        assert(item == i++);
+        assert(listSize(gList) == --j);
+    }
+
+    listClear(gList);
+}
+
+static void ndmPopLast(void) {
+    for (int i = 1; i <= LIST_ITEMS_AMOUNT; i++)
+        listAddBack(gList, (void*) (long) i);
+
+    int i = LIST_ITEMS_AMOUNT, item;
+    while ((item = (int) (long) listPopLast(gList))) {
+        assert(item == i--);
+        assert(listSize(gList) == i);
+    }
+
+    listClear(gList);
+}
+
+static void ndmRemove(void) {
+    for (int i = 1; i <= LIST_ITEMS_AMOUNT; i++)
+        listAddBack(gList, (void*) (long) i);
+
+    listRemove(gList, 0);
+    assert(listSize(gList) == LIST_ITEMS_AMOUNT - 1);
+
+    for (int i = 1; i <= LIST_ITEMS_AMOUNT - 1; i++)
+        assert(listGet(gList, i - 1) == (void*) (long) i + 1);
+
+    listRemove(gList, LIST_ITEMS_AMOUNT - 2);
+    assert(listSize(gList) == LIST_ITEMS_AMOUNT - 2);
+
+    for (int i = 1; i <= LIST_ITEMS_AMOUNT - 2; i++)
+        assert(listGet(gList, i - 1) == (void*) (long) i + 1);
+
+    int i = LIST_ITEMS_AMOUNT - 2;
+    while (listSize(gList)) {
+        listRemove(gList, 0);
+        i--;
+    }
+    assert(!i);
+
+    assert(!listSize(gList));
+}
+
+static void ndmPeek(void) {
+    listAddBack(gList, (void*) 2ul);
+    listAddFront(gList, (void*) 1ul);
+    listAddBack(gList, (void*) 3ul);
+
+    assert(listPeekFirst(gList) == (void*) 1ul);
+    assert(listPeekLast(gList) == (void*) 3ul);
+
+    listClear(gList);
+}
+
+static void ndmSwap(void) {
+    for (int i = 1; i <= LIST_ITEMS_AMOUNT; i++)
+        listAddBack(gList, (void*) (long) i);
+
+    listSwap(gList, 0, LIST_ITEMS_AMOUNT - 1);
+    assert(listGet(gList, 0) == (void*) (long) LIST_ITEMS_AMOUNT);
+    assert(listGet(gList, LIST_ITEMS_AMOUNT - 1) == (void*) (long) 1);
+}
+
+static Compared ndmComparator(const void* const a, const void* const b) {
     return (long) *(void**) a < (long) *(void**) b
         ? COMPARED_LESS
         : (long) *(void**) a > (long) *(void**) b
@@ -11,134 +122,46 @@ static Compared listNoDynamicMemoryComparator(const void* const a, const void* c
             : COMPARED_EQUAL;
 }
 
-static void* listNoDynamicMemoryDuplicator(const void* const value) {
-    return (void*) value;
-}
-
-static void listNoDynamicMemory(void) {
-    const int amount = 10;
-    List* const list = listCreate(DEFAULT_ALLOCATOR, false, nullptr);
-
-    for (int i = 1; i <= amount; i++) {
-        listAddBack(list, (void*) (long) i);
-        assert(listGet(list, i - 1) == (void*) (long) i);
-    }
-
-    listClear(list);
-    assert(!listSize(list));
-    /////////////////
-
-    for (int i = 1; i <= amount; i++)
-        listAddFront(list, (void*) (long) i);
-    for (int i = 1; i <= amount; i++)
-        assert(listGet(list, i - 1) == (void*) (long) (amount - i + 1));
-
-    /////////////////
-    List* const list2 = listCopy(list, false, listNoDynamicMemoryDuplicator);
-    assert(listSize(list) == listSize(list2));
-
-    for (int i = 0; i < amount; i++)
-        assert(listGet(list, i) == listGet(list2, i));
-
-    listDestroy(list2);
-
-    listClear(list);
-    /////////////////
-
-    for (int i = 1; i <= amount; i++)
-        listAddBack(list, (void*) (long) i);
-
-    int ii = 1, item, jj = amount;
-    while ((item = (int) (long) listPopFirst(list))) {
-        assert(item == ii++);
-        assert(listSize(list) == --jj);
-    }
-
-    listClear(list);
-    /////////////////
-
-    for (int i = 1; i <= amount; i++)
-        listAddBack(list, (void*) (long) i);
-
-    ii = amount;
-    while ((item = (int) (long) listPopLast(list))) {
-        assert(item == ii--);
-        assert(listSize(list) == ii);
-    }
-
-    listClear(list);
-    /////////////////
-
-    for (int i = 1; i <= amount; i++)
-        listAddBack(list, (void*) (long) i);
-
-    listRemove(list, 0);
-    assert(listSize(list) == amount - 1);
-
-    for (int i = 1; i <= amount - 1; i++)
-        assert(listGet(list, i - 1) == (void*) (long) i + 1);
-
-    listRemove(list, amount - 2);
-    assert(listSize(list) == amount - 2);
-
-    for (int i = 1; i <= amount - 2; i++)
-        assert(listGet(list, i - 1) == (void*) (long) i + 1);
-
-    ii = amount - 2;
-    while (listSize(list)) {
-        listRemove(list, 0);
-        ii--;
-    }
-    assert(!ii);
-
-    /////////////////
-    assert(!listSize(list));
-
-    listAddBack(list, (void*) 2ul);
-    listAddFront(list, (void*) 1ul);
-    listAddBack(list, (void*) 3ul);
-
-    assert(listPeekFirst(list) == (void*) 1ul);
-    assert(listPeekLast(list) == (void*) 3ul);
-
-    listClear(list);
-    /////////////////
-
-    for (int i = 1; i <= amount; i++)
-        listAddBack(list, (void*) (long) i);
-
-    listSwap(list, 0, amount - 1);
-    assert(listGet(list, 0) == (void*) (long) amount);
-    assert(listGet(list, amount - 1) == (void*) (long) 1);
-
-    /////////////////
-
+static void ndmSort(void) {
     srand(time(nullptr));
 
-    for (int i = 1; i <= amount; i++)
-        listSwap(list, rand() % amount, rand() % amount);
+    for (int i = 1; i <= LIST_ITEMS_AMOUNT; i++)
+        listSwap(gList, rand() % LIST_ITEMS_AMOUNT, rand() % LIST_ITEMS_AMOUNT);
 
-    listQSort(list, listNoDynamicMemoryComparator);
+    listQSort(gList, ndmComparator);
 
-    for (int i = 1, j = 0, k; i <= amount; i++) {
-        k = (int) (long) listGet(list, i - 1);
+    for (int i = 1, j = 0, k; i <= LIST_ITEMS_AMOUNT; i++) {
+        k = (int) (long) listGet(gList, i - 1);
         assert(k >= j);
         j = k;
     }
-
-    /////////////////
-
-    for (int value = 1; value <= amount; value++) {
-        for (int index = 0; index < amount; index++) {
-            void* const xvalue = (void*) (long) value;
-            if (listGet(list, index) == xvalue)
-                assert(listBinarySearch(list, &xvalue, listNoDynamicMemoryComparator) == xvalue);
-        }
-    }
-
-    listDestroy(list);
 }
 
-void testCollections(void) {
-    listNoDynamicMemory();
+static void ndmBinarySearch(void) {
+    for (int value = 1; value <= LIST_ITEMS_AMOUNT; value++) {
+        for (int index = 0; index < LIST_ITEMS_AMOUNT; index++) {
+            void* const xvalue = (void*) (long) value;
+            if (listGet(gList, index) == xvalue)
+                assert(listBinarySearch(gList, &xvalue, ndmComparator) == xvalue);
+        }
+    }
+}
+
+static void ndmQuit(void) {
+    listDestroy(gList);
+}
+
+void testCollectionsList(void) {
+    ndmInit();
+    ndmAddBack();
+    ndmAddFront();
+    ndmCopy();
+    ndmPopFirst();
+    ndmPopLast();
+    ndmRemove();
+    ndmPeek();
+    ndmSwap();
+    ndmSort();
+    ndmBinarySearch();
+    ndmQuit();
 }
--- tests/testMain.c
+++ tests/testMain.c
@@ -3,14 +3,14 @@
 #include "../src/defs.h"
 
 void testCrypto(void);
-void testCollections(void);
+void testCollectionsList(void);
 
 int main(const int argc, const char* const* const argv) {
     assert(argc == 2);
 
     switch (atoi(argv[1])) {
         case 0: testCrypto(); break;
-        case 1: testCollections(); break;
+        case 1: testCollectionsList(); break;
         case 2: break;
         case 3: break;
         default: assert(false);
